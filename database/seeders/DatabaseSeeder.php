8012d5f96e777fba100a98e7d84a143f01079fad
Abdullah Al Wafi <abdullahalwafi16@gmail.com>
2024-06-03 09:05:16 +0700

    Feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\OpenResto;
+use App\Models\Table;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,16 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(5)->create();
-        \App\Models\Categories::factory(5)->create();
-        \App\Models\Products::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+        // \App\Models\Categories::factory(5)->create();
+        // \App\Models\Products::factory(5)->create();
+        // OpenResto::create([
+        //     'is_open' => 1,
+        // ]);
 
+        for ($i = 1; $i < 25; $i++) {
+            Table::create([
+                'name' => "Meja $i",
+                'status' => "Available",
+            ]);
+        }
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            UserSeeder::class,
-        ]);
+        // $this->call([
+        //     UserSeeder::class,
+        // ]);
     }
 }
